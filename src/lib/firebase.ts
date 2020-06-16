5f1ffcead9c0dc74d5ad95069ccb03b393894613
Matt Wang <matt@matthewwang.me>
2020-06-15 19:06:21 -0700

    add firebase defaults

---
+++ src/lib/firebase.ts
@@ -0,0 +1,13 @@
+import * as firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyBudAEATUpeECdlMSVktpuwXSMTB3jXAyk",
+    authDomain: "roebling-e3a34.firebaseapp.com",
+    databaseURL: "https://roebling-e3a34.firebaseio.com",
+    projectId: "roebling-e3a34",
+    storageBucket: "roebling-e3a34.appspot.com",
+    messagingSenderId: "685150714993",
+    appId: "1:685150714993:web:fd9d8e60878ee10b216fd0"
+};
+
+firebase.initializeApp(config);
